fd127aee58344f486def5ac274613d6f4e424bb5
Bini7yam <134131394+Bini7yam@users.noreply.github.com>
2025-07-25 16:50:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 
+const inputField = document.getElementById("inputField")
+const searchButton = document.getElementById("searchButton")
+
+const ul = document.getElementById('ul')
+console.log(ul)
+
+function clicked(e){
+    textContent=inputField.value.trim()
+    if(!textContent){
+        alert(textContent)
+        return;
+    }
+    ul.innerHTML+=" <li><span class=\"check\" data-e=\"1\"></span>"+textContent+"<span class=\"terminate\">x</span></li>"
+    inputField.value=''
+}
+
+searchButton.addEventListener('click', clicked)
+inputField.addEventListener('keydown',function(e){
+    if(e.key==='Enter')clicked(e)
+})
+
+ul.addEventListener('click', function(e){
+    const target=e.target
+
+    console.log(target.classList.contains('check'))
+    if(target.classList.contains('check')){
+        if(target.dataset.e=='0'){
+            target.style.backgroundImage='none'
+            target.parentElement.style.textDecoration='none'
+            target.dataset.e='1'
+        }else{
+            target.style.backgroundImage='url(rg.avif)'
+            target.parentElement.style.textDecoration='line-through'
+            target.dataset.e='0'
+        }
+        console.log(getComputedStyle(target).backgroundImage,target.dataset.e)
+    }
+    if(target.classList.contains('terminate')){
+        console.log(target.parentElement.remove())
+    }
+})
